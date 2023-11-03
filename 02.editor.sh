50fd074aa06a43768d744204cc4f929e74adf7b6
github@wolfhouse.me <Thomas G Wolf>
2023-11-03 19:34:40 +0000

    Added hx as an editor option

--- 02.editor.sh
+++ 02.editor.sh
@@ -1,5 +1,5 @@
 # Set the default editor based on availability and priority
-for editor in helix nvim vim; do
+for editor in helix hx nvim vim; do
   if command -v $editor >/dev/null 2>&1; then
     export EDITOR=$(command -v $editor)
     export VISUAL=$(command -v $editor)
