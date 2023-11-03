30b2f1b07016c1166a4bee7a04a9dbdb6f86bee2
Thomas G Wolf <tgreyw@gmail.com>
2023-11-03 13:51:16 -0600

    Will now add the helix config files if they dont exist

--- 03.helix.sh
+++ 03.helix.sh
@@ -7,3 +7,9 @@ fi
 if which helix >/dev/null 2>/dev/null; then 
   export HELIX_RUNTIME="${HOME}/.config/helix/runtime"
 fi
+
+for config_file in "$(dirname "${BASH_SOURCE[0]}")"/configs/helix/*; do 
+  if ! -e "${HOME}/.config/$(basename "${config_file}")"; then 
+    cp -r "${config_file}" "${HOME}/.config/$(basename "${config_file}")"
+  fi
+done
