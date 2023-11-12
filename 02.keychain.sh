178928720a96e88791aa0dfccdd0bdd5c328429f
Thomas G Wolf <tgreyw@gmail.com>
2023-11-11 21:58:00 -0700

    Enabled the creation of ~/.local if it doesn't exist

--- 02.keychain.sh
+++ 02.keychain.sh
@@ -1,5 +1,8 @@
 # Initialize the keychain, if available
-if command -v keychain >/dev/null 2>&1; then
+if  command -v keychain >/dev/null 2>&1; then
+  # Make sure there is a user ~/.local dir
+  mkdir -p "${HOME}/.local"
+  
   eval $(keychain --eval --nogui --noask --quiet \
     --dir "${HOME}/.local/keychain" \
     "$(find ~/.ssh -name 'id_*' ! -name '*.pub')" \
