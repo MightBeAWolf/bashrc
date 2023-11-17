c2ab8e9e3bed792364f55537e21752ef9d4eb438
Thomas G Wolf <5043711+MightBeAWolf@users.noreply.github.com>
2023-11-17 08:39:14 -0700

    Removed configs from the bashrc, as these are now there own repo

--- 03.helix.sh
+++ 03.helix.sh
@@ -8,8 +8,3 @@ if which helix >/dev/null 2>/dev/null; then
   export HELIX_RUNTIME="${HOME}/.config/helix/runtime"
 fi
 
-for config_file in "$(dirname "${BASH_SOURCE[0]}")"/configs/helix/*; do 
-  if ! [[ -e "${HOME}/.config/helix/$(basename "${config_file}")" ]]; then
-    cp -r "${config_file}" "${HOME}/.config/helix/$(basename "${config_file}")"
-  fi
-done
