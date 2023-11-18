5be0f0363336b88edd40196a0fb547530d129f99
Thomas G Wolf <5043711+MightBeAWolf@users.noreply.github.com>
2023-11-18 11:31:04 -0700

    Fixed pager

--- 02.pager.sh
+++ 02.pager.sh
@@ -1,4 +1,4 @@
 if command -v bat >/dev/null 2>&1; then
   export PAGER="$(command -v bat)"
-  export MANPAGER="sh -c 'col -bx | $(command -v $editor) -l man -p'"
+  export MANPAGER="sh -c 'col -bx | $(command -v bat) -l man -p'"
 fi
