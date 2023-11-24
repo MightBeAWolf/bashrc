94bfb02b0e22f828683fd1ae5a96ddc51cea1817
Thomas G Wolf <5043711+MightBeAWolf@users.noreply.github.com>
2023-11-24 11:34:53 -0700

    Configured fzf

--- 00.paths.sh
+++ 00.paths.sh
@@ -2,6 +2,7 @@
 # Iterate over prospective PATH additions and add them if they exist
 prospective_path_additions=(
   "${HOME}/.local/bin"
+  "${HOME}/.fzf/bin"
 )
 for dir in "${prospective_path_additions[@]}"; do
   if [[ -d "$dir" ]]; then
---
+++ 03.fzf.sh
@@ -0,0 +1,12 @@
+# Setup fzf
+# ---------
+# NOTE that the PATH has been set for fzf in 
+# 00.paths.sh
+
+# Auto-completion
+# ---------------
+source "${HOME:?}/.fzf/shell/completion.bash"
+
+# Key bindings
+# ------------
+source "${HOME:?}/.fzf/shell/key-bindings.bash"
