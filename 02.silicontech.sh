84a466551c56e77eded8d955547ecbba318dd828
Thomas G Wolf <gwolf@silicontechnologiesinc.com>
2024-03-19 09:30:19 -0600

    Enabled eda tool environment aliases for work

--- 02.silicontech.sh
+++ 02.silicontech.sh
@@ -5,3 +5,17 @@ if [[ -f /home/share/Environments/users/default.bashrc ]]; then
 	source /home/share/Environments/users/default.bashrc
 fi
 
+for env_path in /opt/eda/tools/*/*/*/envs/env.sh; do
+	# Extract the directory name of the third star in the glob pattern
+	tool_version=$(basename $(dirname $(dirname $env_path)))
+	tool_name=$(basename $(dirname $(dirname $(dirname $env_path))))
+	if grep -F "${tool_name}" <<< "${tool_version}"; then
+		# Create an alias by concatenating the directory name with '-ENV'
+		# and sourcing the env.sh file
+		alias "env-${tool_version,,}"="source $env_path"
+	else
+		alias "env-${tool_name,,}-${tool_version,,}"="source $env_path"
+	fi
+done
+
+
