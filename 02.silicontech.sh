b3031b0ea639509b0bac541d7a8df97ec4ff2c77
Thomas G Wolf <gwolf@silicontechnologiesinc.com>
2024-08-06 08:30:37 -0600

    Added some file checking to 02.silicontech.us

--- 02.silicontech.sh
+++ 02.silicontech.sh
@@ -5,26 +5,30 @@ if [[ -f /home/share/Environments/users/default.bashrc ]]; then
 	source /home/share/Environments/users/default.bashrc
 fi
 
-for env_path in /opt/eda/tools/*/*/*/envs/env.sh; do
-	# Extract the directory name of the third star in the glob pattern
-	tool_version=$(basename $(dirname $(dirname $env_path)))
-	tool_name=$(basename $(dirname $(dirname $(dirname $env_path))))
-	if grep -F "${tool_name}" <<< "${tool_version}" >/dev/null 2>/dev/null; then
-		# Create an alias by concatenating the directory name with '-ENV'
-		# and sourcing the env.sh file
-		alias "env-${tool_version,,}"="source $env_path"
-	else
-		alias "env-${tool_name,,}-${tool_version,,}"="source $env_path"
-	fi
-done
+if [[ -d /opt/eda/tools ]]; then
+	for env_path in /opt/eda/tools/*/*/*/envs/env.sh; do
+		# Extract the directory name of the third star in the glob pattern
+		tool_version=$(basename $(dirname $(dirname $env_path)))
+		tool_name=$(basename $(dirname $(dirname $(dirname $env_path))))
+		if grep -F "${tool_name}" <<< "${tool_version}" >/dev/null 2>/dev/null; then
+			# Create an alias by concatenating the directory name with '-ENV'
+			# and sourcing the env.sh file
+			alias "env-${tool_version,,}"="source $env_path"
+		else
+			alias "env-${tool_name,,}-${tool_version,,}"="source $env_path"
+		fi
+	done
+fi
 
-# Add an alias for opening the Cadence documentation and skill API finder.
-# Get the latest cadence version, as we'll use it for the documentation.
-latest_cadence="$( \
-	fd -td --exact-depth 2 IC /opt/eda/tools/Cadence/ -x stat --format '%Y:%n' \
-	| sort -n \
-	| tail -1 \
-	| cut -d: -f2 \
-	)"
-alias "cadence-docs"="(source ${latest_cadence:?}/envs/env.sh; \${CDS}/bin/cda & \${CDS}/bin/cdsFinder &) &"
+if [[ -d /opt/eda/tools/Cadence ]]; then
+	# Add an alias for opening the Cadence documentation and skill API finder.
+	# Get the latest cadence version, as we'll use it for the documentation.
+	latest_cadence="$( \
+		fd -td --exact-depth 2 IC /opt/eda/tools/Cadence/ -x stat --format '%Y:%n' \
+		| sort -n \
+		| tail -1 \
+		| cut -d: -f2 \
+		)"
+	alias "cadence-docs"="(source ${latest_cadence:?}/envs/env.sh; \${CDS}/bin/cda & \${CDS}/bin/cdsFinder &) &"
+fi
 
