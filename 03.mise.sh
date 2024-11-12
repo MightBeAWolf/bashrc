49681868f609a054d9ee70d6614d084624b5bd47
Thomas G Wolf <gwolf@silicontechnologiesinc.com>
2024-11-12 12:21:24 -0700

    Removed hardcoded home dir

--- 03.mise.sh
+++ 03.mise.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 if [[ -e "${HOME}/.local/bin/mise" ]]; then
-  eval "$(/home/gwolf/.local/bin/mise activate bash)"
+  eval "$("${HOME}/.local/bin/mise" activate bash)"
+else
+  alias mise="echo 'Install mise using \"curl https://mise.run | sh\"' "
 fi
