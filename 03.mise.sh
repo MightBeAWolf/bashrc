84891153a239920fdd1d480e1f2f3f532313f2c2
Thomas G. Wolf <github@wolfhouse.me>
2024-11-29 14:29:54 -0800

    Added mise autocomplete setup

--- 03.mise.sh
+++ 03.mise.sh
@@ -2,6 +2,12 @@
 
 if [[ -e "${HOME}/.local/bin/mise" ]]; then
   eval "$("${HOME}/.local/bin/mise" activate bash)"
+  if ! command -v usage; then
+    mise use -g usage
+  fi
+  if ! command -v _mise; then
+    eval "$(mise completion bash)"
+  fi
 else
   alias mise="echo 'Install mise using \"curl https://mise.run | sh\"' "
 fi
