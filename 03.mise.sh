9324a8ade6053c53fc867518ca39d369862cdd21
MightBeAWolf <github@wolfhouse.me>
2024-12-01 12:01:17 -0800

    Made mise init silent

--- 03.mise.sh
+++ 03.mise.sh
@@ -2,10 +2,10 @@
 
 if [[ -e "${HOME}/.local/bin/mise" ]]; then
   eval "$("${HOME}/.local/bin/mise" activate bash)"
-  if ! command -v usage; then
+  if ! command -v usage >/dev/null; then
     mise use -g usage > /dev/null
   fi
-  if ! command -v _mise; then
+  if ! command -v _mise >/dev/null; then
     eval "$(mise completion bash)"
   fi
 else
